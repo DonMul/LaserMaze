bab298948c88e31b02ed2a29b626e0c25f5f84fe
Joost Mul <joost@jmul.net>
2019-10-09 23:42:49 +0200

    Update Controller.ino

--- Controller.ino
+++ Controller.ino
@@ -1,16 +1,10 @@
 #include <Wire.h>
 #include <TM1637Display.h>
-#include "DS3231.h"
+#include <DS3231.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-// End button
 #define pushButton D8
-
-// WiFi
-const IPAddress apIP = IPAddress(192, 168, 4, 1);
-const char* ssid = "laser";
-const char* password = "lasermaze";
 
 // TM1637 Display
 #define CLK D3
@@ -20,26 +14,31 @@ TM1637Display display(CLK, DIO);
 ESP8266WebServer server(80);
 
 // RTC
-RTClib RTC;
+RTClib  rtc;
 
 int startTimestamp = 0;
 int curTime = 0;
 bool timerRunning = true;
 
+const IPAddress apIP = IPAddress(42, 42, 42, 42);
+
 void setup() {
   Serial.begin(9600);
   delay(1000);
   Wire.begin();
-  
+
   display.showNumberDecEx(0, (0x80 >> 1), true);
   display.setBrightness(7, true);
-  DateTime now = RTC.now();
+  DateTime now = rtc.now();
   startTimestamp = now.unixtime();
 
-  WiFi.mode(WIFI_AP);
+  WiFi.mode(WIFI_AP_STA);
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
-  WiFi.softAP(ssid, password);
+  boolean result = WiFi.softAP("laser", "lasermaze");
   
+  Serial.print("AP IP address: ");
+  Serial.println(WiFi.softAPIP());
+
   server.on("/start", handleStart);
   server.on("/stop", handleStop);
   server.on("/punish", handlePunish);
@@ -52,8 +51,10 @@ void setup() {
 void loop() {
   server.handleClient();
   if (timerRunning == true) {
-    DateTime now = RTC.now();
+    DateTime now = rtc.now();
     curTime = now.unixtime() - startTimestamp;
+
+    Serial.println(now.unixtime());
 
     int stateButton = digitalRead(pushButton);
     if (stateButton == 1) {
@@ -65,23 +66,25 @@ void loop() {
 }
 
 void handleStart() {
+  server.send(200, "text/plain", "start");
+  Serial.println("Start");
+
   if (timerRunning == true) {
     return;
   }
-  DateTime now = RTC.now();
+  DateTime now = rtc.now();
   startTimestamp = now.unixtime();
   timerRunning = true;
 }
 
 void handleStop() {
-  if (timerRunning == false) {
-    return;
-  }
-  
+  server.send(200, "text/plain", "stop");
   timerRunning = false;
 }
 
+
 void handlePunish() {
+  server.send(200, "text/plain", "punish");
   if (timerRunning == false) {
     return;
   }
