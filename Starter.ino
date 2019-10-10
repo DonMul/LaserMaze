90deda0fb0adc974c167df89d04b0fe6b6190d9e
Joost Mul <joost@jmul.net>
2019-10-10 10:46:10 +0200

    Update Starter.ino

--- Starter.ino
+++ Starter.ino
@@ -1,13 +1,8 @@
 #include <Arduino.h>
 
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
-
 #include <ESP8266HTTPClient.h>
 
-#define USE_SERIAL Serial
-
-ESP8266WiFiMulti WiFiMulti;
 // Start button
 #define pushButton D8
 
@@ -17,53 +12,51 @@ const char* password = "lasermaze";
 int previousState = 0;
 
 void setup() {
-    USE_SERIAL.begin(115200);
+    Serial.begin(115200);
     WiFi.begin(ssid, password);
-    delay(1000);
+    
+    tryWifiConnect();
+
+    pinMode(pushButton, INPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void tryWifiConnect() {
+  if (WiFi.status() != WL_CONNECTED) {
     Serial.print("Connecting to Access point..");
+    WiFi.begin(ssid, password);
+    
     while (WiFi.status() != WL_CONNECTED) {
       Serial.print(".");
       delay(100);
     }
-    delay(100);
+    
     Serial.println("connected");
-
-    pinMode(pushButton, INPUT);
-    pinMode(BUILTIN_LED, OUTPUT);
+  }
 }
 
 void loop() {
-    digitalWrite(BUILTIN_LED, HIGH);
+    tryWifiConnect();
+    digitalWrite(LED_BUILTIN, HIGH);
+    
     int stateButton = digitalRead(pushButton);
 
     if (stateButton != previousState) {
       previousState = stateButton;
 
       if (stateButton == HIGH) {
+        digitalWrite(LED_BUILTIN, LOW);
+        Serial.println("Send start");
+        
         HTTPClient http;
-
-        USE_SERIAL.print("[HTTP] begin...\n");
-        http.begin("42.42.42.42", 80, "/start"); //HTTP
-
-        USE_SERIAL.print("[HTTP] GET...\n");
-        // start connection and send HTTP header
+        http.begin("42.42.42.42", 80, "/start");
         int httpCode = http.GET();
-
-        // httpCode will be negative on error
-        if(httpCode > 0) {
-            // HTTP header has been send and Server response header has been handled
-            USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
-
-            // file found at server
-            if(httpCode == HTTP_CODE_OK) {
-                String payload = http.getString();
-                USE_SERIAL.println(payload);
-            }
-        } else {
-            USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-        }
-
         http.end();
+        
+        Serial.println("Start send");
+        digitalWrite(LED_BUILTIN, HIGH);
       }
     }
+    
+    digitalWrite(LED_BUILTIN, LOW);
 }
